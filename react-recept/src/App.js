b32ff6e54cf85c03fecc8eb53d510fa1a8466976
Luna Bos <lunabos@lunas-mbp.home>
2020-06-23 21:03:43 +0200

    laatste poging

--- react-recept/src/App.js
+++ react-recept/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import Header from './components/layout/Header'; 
 import Steps from './components/Steps';
 import AddStep from './components/AddStep';
